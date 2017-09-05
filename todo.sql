fbf2cb1346ef8ac1ba2d9ca2e5976ad3d2a741a1
Keith Canty <keith.curtis.canty@gmail.com>
2017-09-05 15:45:19 -0400

    second commit

--- todo.sql
+++ todo.sql
@@ -10,13 +10,13 @@ CREATE TABLE todos(
   completed_at TIMESTAMP NULL
  );
 
-INSERT INTO todos (title, details, priority, created_at) VALUES ('Walk the dog', 'Take Duke for a long walk', 3, 'now';
+INSERT INTO todos (title, details, priority, created_at) VALUES ('Walk the dog', 'Take Duke for a long walk', 3, 'now');
 INSERT INTO todos (title, details, priority, created_at) VALUES ('Make breakfast', 'Crack 3 eggs', 5, 'now');
 INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Get haircut', 'Drive to barber shop', 4, 'now', 'now');
 INSERT INTO todos (title, details, priority, created_at) VALUES ('Vacuum the house', 'Do not forget to move the couch', 1, 'now');
 INSERT INTO todos (title, details, priority, created_at) VALUES ('Bake cheesecake', 'Decide between chocolate or traditional', 2, 'now');
 
-SELECT title FROM todos WHERE completed_at = NULL;
+SELECT title FROM todos WHERE completed_at IS NULL;
 SELECT title FROM todos WHERE priority > 1;
 
 UPDATE todos SET details = 'bake at 320 degrees' WHERE id = 5;
